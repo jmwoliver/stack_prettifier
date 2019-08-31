e1f9f74aab94598ef65a2d0bfd91578868d9ce09
Jacob Woliver <jacobwoliver@protonmail.com>
2019-08-31 11:48:06 -0400

    Initial commit

---
+++ stack_prettifier.js
@@ -0,0 +1,5 @@
+document.getElementById("sidebar").style.display = 'none';
+document.getElementById("left-sidebar").style.display = 'none';
+document.getElementById("footer").style.display = 'none';
+document.getElementById("js-gdpr-consent-banner").style.display = 'none';
+mainbar.style.width = 100.138 + '%';
