c0fa3722a9d7bdfe19e33afb84c40b400132d4ad
Jacob Woliver <jacobwoliver@protonmail.com>
2019-09-02 12:28:09 -0400

    updated to expand content

--- stack_prettifier.js
+++ stack_prettifier.js
@@ -1,5 +1,16 @@
-document.getElementById("sidebar").style.display = 'none';
-document.getElementById("left-sidebar").style.display = 'none';
-document.getElementById("footer").style.display = 'none';
-document.getElementById("js-gdpr-consent-banner").style.display = 'none';
-mainbar.style.width = 100.138 + '%';
+var sidebar = document.getElementById("sidebar");
+sidebar.parentNode.removeChild(sidebar);
+
+var leftsidebar = document.getElementById("left-sidebar");
+leftsidebar.parentNode.removeChild(leftsidebar);
+
+var banner = document.getElementById("js-gdpr-consent-banner");
+banner.parentNode.removeChild(banner);
+
+var footer = document.getElementById("footer");
+footer.parentNode.removeChild(footer);
+
+var mainbar = document.getElementById("mainbar");
+var width = mainbar.offsetWidth;
+var newWidth = width * .138;
+mainbar.style.width = newWidth + '%';
